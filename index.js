047107a3fd5c83e286bcc276115902105b5b841c
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-08-30 20:57:30 -0700

    add google oauth

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express'),
-      port    = process.env.PORT || 3000,
-      app     = express();
+const express        = require('express'),
+      passport       = require('passport'),
+      GoogleStrategy = require('passport-google-oauth20').Strategy,
+      keys           = require('./config/keys'),
+      port           = process.env.PORT || 3000,
+      app            = express();
 
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSECRET,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
+
+
 
 app.listen(port, () => {
   console.log(`Application running on port ${port}`);
