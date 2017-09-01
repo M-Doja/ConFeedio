e5f56e8ddc4ca67e92a6f6f962d07f9b60d1a3ef
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-08-31 18:49:17 -0700

    installed mongodb and mongoose

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express'),
-      port    = process.env.PORT || 3000,
-      app     = express();
+const express  = require('express'),
+      mongoose = require('mongoose'),
+      port     = process.env.PORT || 3000,
+      keys     = require('./config/keys'),
+      app      = express();
+
+
+// DATABASE CONNECTION
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.testDB_URI, {
+  useMongoClient: true
+}, (err, db) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Now connected to DB');
+  db = db;
+});
 
 require('./services/passport');
 require('./routes/authRoutes')(app);
