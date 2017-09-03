b0818685a8dd6d23434b080bfa52919cd1e988d8
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-09-02 20:31:54 -0700

    added routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <h2>Header</h2>
+  )
+}
+
+const Dashboard = () => {
+  return (
+    <h2>Dashboard</h2>
+  )
+}
+
+const SurveyNew = () => {
+  return (
+    <h2>Survey New</h2>
+  )
+}
+
+const Landing = () => {
+  return (
+    <h2>Landing</h2>
+  )
+}
 
 const App = () => {
   return (
-    <div>
-      Hi There!
+    <div className="">
+      <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveyNew} />
+      </div>
+      </BrowserRouter>
     </div>
   );
 };
