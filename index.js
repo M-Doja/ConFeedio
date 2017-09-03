29308d7e313f83becbbdbab2a0b98cb177c48d4f
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-09-02 23:22:59 -0700

    push to heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('./services/passport');
 
 // DATABASE CONNECTION
 // mongoose.Promise = global.Promise;
-mongoose.connect(keys.testDB_URI, {
+mongoose.connect(keys.prodDB_URI, {
   useMongoClient: true
 }, (err, db) => {
   if (err) {
