6b80648adfe430b69966cb3c73f8c24c2cf1584c
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-09-05 19:00:18 -0700

    added survey form and apikey

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
+export default ({input, label}) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
+}
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
   render() {
     return (
       <div>
         <h2>Survey New</h2>
-      </div>  
+        <SurveyForm />
+      </div>
     );
   }
 }
